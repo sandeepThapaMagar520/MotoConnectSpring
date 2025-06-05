9f1ab98e6bdcc8fe464a8326ed4b0feb9f8aa29f
Sandeep-Thapa-Magar <sandeepthapa520@gmail.com>
2025-06-05 22:36:07 +0545

    initial commit

---
+++ src/main/java/com/project/motoconnect/MotoconnectApplication.java
@@ -0,0 +1,13 @@
+package com.project.motoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/motoconnect/MotoconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.motoconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
